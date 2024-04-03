ca7b06cb986c5b2820f5f9a4e64060d2764ba0a6
Johnny <jmnaze123@gmail.com>
2024-04-03 16:08:05 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# CHECK FOR DEBUG MODE
-
 if [ "$1" = "--debug" ]; then
     echo "Running in debug mode"
     CLA="--debug"
@@ -9,36 +7,5 @@ else
     CLA=""
 fi
 
-# CHECK CWD
 
-DIR=$(pwd | grep -o '[^/]*$')
-
-if [ $DIR != "Zephyr" ]; then
-    echo "Please run the script from the Zephyr directory"
-    exit 1
-fi
-
-# CHECK FOR PYTHON
-
-HAS_PYTHON=false
-PYTHON3=false
-
-if [ -x "$(command -v python3)" ]; then
-    HAS_PYTHON=true
-    PYTHON3=true
-elif [ -x "$(command -v python)" ]; then
-    HAS_PYTHON=true
-fi
-
-if [ $HAS_PYTHON = false ]; then
-    echo "Python is not installed"
-    exit 1
-fi
-
-# RUN APP
-
-if [ $PYTHON3 = true ]; then
-    python3 zephyr/server.py $CLA
-else
-    python zephyr/server.py $CLA
-fi
+python zephyr/server.py $CLA
