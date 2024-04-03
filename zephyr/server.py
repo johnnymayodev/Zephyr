a496d5b8c63aefdf5bbf8375d03bedb1d6d51fbc
Johnny <jmnaze123@gmail.com>
2024-04-03 16:08:21 -0400

    Update server.py

--- zephyr/server.py
+++ zephyr/server.py
@@ -2,23 +2,28 @@ import sys
 from flask import Flask, render_template
 from libraries.compiler import compile
 
-app = Flask(__name__, template_folder='./', static_folder='../app/')
-DEV_PORT = 3000
+app = Flask(__name__, template_folder="./", static_folder="../app/")
 
-DEBUG = '--debug' in sys.argv
-print('DEBUG:', DEBUG)
+DEBUG = "--debug" in sys.argv
+print("DEBUG:", DEBUG)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    if DEBUG: # If in debug mode, compile the files before rendering the template
+    if DEBUG:  # If in debug mode, compile the files before rendering the template
         compile()
-    return render_template('.index.html')
+    return render_template(".index.html")
+
 
 def main():
     if not DEBUG: # If not in debug mode, compile the files before running the app
         compile()
-    app.run(port=DEV_PORT, debug=DEBUG)
+        port = 80
+    else: # If in debug mode, set the port to 8080
+        port = 8080
+
+    app.run(port=port, debug=DEBUG)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
